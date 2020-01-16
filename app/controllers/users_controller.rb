33bfc44d61cebd5c927170cf189a3608e564831c
Roheem Olayemi <abdurroheem20@gmail.com>
2020-01-16 18:50:42 +0100

    created the view form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,21 @@ class UsersController < ApplicationController
     end
 
     def create
+        # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+        @user = User.new(user_params)
+
+  if @user.save
+    redirect_to new_user_path
+  else
+    render :new
+  end
+    end
+
+    private  # Best to make helper methods like this one private
+
+    # gives us back just the hash containing the params we need to
+    # to create or update a user
+    def user_params
+      params.require(:user).permit(:useraname,:email,:password)
     end
 end
