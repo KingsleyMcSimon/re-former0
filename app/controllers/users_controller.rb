867a22a8d1f491554f263a228082b23cbfe4b581
kingsley McSimon Ogbonna <kingsley.mcsimon@gmail.com>
2020-01-18 22:46:52 -0800

    users_controller, user, routes, and shema stickler error fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,25 @@
 class UsersController < ApplicationController
-
-    def new
+  def new
       @user = User.new
     end
 
-    def edit
-        @user = User.find(params[:id])
+  def edit
+  @user = User.find(params[:id])
     end
 
-    def update
-        @user = User.find(params[:id])
-        if @user.update(user_params)
+  def update
+    @user = User.find(params[:id])
+
+  if @user.update_attributes(user_param)
             redirect_to new_user_path
-        else
+  else
             render :new
         end
     end
 
-    def create
-        # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-        @user = User.new(user_params)
+  def create
+  # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+  @user = User.new(user_params)
 
   if @user.save
     redirect_to new_user_path
@@ -28,11 +28,11 @@ class UsersController < ApplicationController
   end
     end
 
-    private  # Best to make helper methods like this one private
+  private  # Best to make helper methods like this one private
 
-    # gives us back just the hash containing the params we need to
-    # to create or update a user
-    def user_params
+  # gives us back just the hash containing the params we need to
+  # to create or update a user
+  def user_params
       params.require(:user).permit(:useraname,:email,:password)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ApplicationRecord
-    has_secure_password
-    validates :username, presence: true
-    validates :email, presence: true
-    validates :password, presence: true
+  has_secure_password
+  validates :username, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
 end
-
-
