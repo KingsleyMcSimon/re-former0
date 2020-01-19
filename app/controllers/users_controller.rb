1b24fc48cd033f92bba65399328c6eaf9f7526f5
kingsley McSimon Ogbonna <kingsley.mcsimon@gmail.com>
2020-01-18 23:45:43 -0800

    More stickler.yml issues fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def new
-      @user = User.new
+    @user = User.new
   end
 
   def edit
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
 
-  if @user.update_attributes(user_param)
+    if @user.update_attributes(user_param)
     redirect_to new_user_path
   else
     render :new
@@ -18,21 +18,21 @@ class UsersController < ApplicationController
   end
 
   def create
-  # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(user_params)
 
-  if @user.save
+    if @user.save
     redirect_to new_user_path
   else
     render :new
   end
   end
 
-  private  # Best to make helper methods like this one private
+  private # Best to make helper methods like this one private
 
   # gives us back just the hash containing the params we need to
   # to create or update a user
   def user_params
-    params.require(:user).permit(:useraname, :email,:password)
+    params.require(:user).permit(:useraname, :email, :password)
   end
 end
