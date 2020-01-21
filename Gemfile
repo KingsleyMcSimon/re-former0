f1f52c81f205403bfe18cb10b2e51fe17b898026
Roheem Olayemi <abdurroheem20@gmail.com>
2020-01-21 18:00:51 +0100

    corrected the linting errors

--- Gemfile
+++ Gemfile
@@ -11,8 +11,6 @@ gem 'rails', '~> 6.0.2', '>= 6.0.2.1'
 gem 'puma', '~> 4.1'
 # Use SCSS for stylesheets
 
-gem 'rubocop'
-
 gem 'sass-rails', '>= 6'
 # Transpile app-like JavaScript. Read more: https://github.com/rails/webpacker
 gem 'webpacker', '~> 4.0'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,23 +9,21 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    
     if @user.update_attributes(user_param)
-    redirect_to new_user_path
-  else
-    render :new
-  end
+      redirect_to new_user_path
+    else
+      render :new
+    end
   end
 
   def create
     # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(user_params)
-    
     if @user.save
-    redirect_to new_user_path
-  else
-    render :new
-  end
+      redirect_to new_user_path
+    else
+      render :new
+    end
   end
 
   private # Best to make helper methods like this one private
